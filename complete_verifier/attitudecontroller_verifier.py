2f2f2b4095b23f478f69358d7d33a234d78f77d8
zwc662 <zwc662@gmail.com>
2021-11-28 18:18:38 -0500

    upda

--- complete_verifier/attitudecontroller_verifier.py
+++ complete_verifier/attitudecontroller_verifier.py
@@ -102,9 +102,11 @@ def main():
             print("Attitude controller's output {}".format(u_pred))
             for idx in range(model_ori.output_size):
                 model_ori.filter(idx, arguments.Config["general"]["device"])
+                model_ori = model_ori.to(arguments.Config["general"]["device"])
                 u_pred = model_ori(x)
                 print("Attitude controller filtered {}th output {}".format(idx, u_pred))
                 model_ori.filter(device = arguments.Config["general"]["device"])
+                model_ori.to(arguments.Config["general"]["device"])
           
                 
  
